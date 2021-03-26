f20af23d6841d1167c5f5f6e6f3ee9ea0afb786c
Jose Caloca <jose.caloca3@gmail.com>
2021-03-26 21:23:10 +0100

    strings

--- string_vars.py
+++ string_vars.py
@@ -1,1 +1,14 @@
-s = 'abcd'
+s = 'abcdefaa'
+
+print(len(s))
+print(s[1:])
+print(s[1:3])
+print(s[:2])
+print(s[-2:])
+print(s[-3:])
+
+s2 = 'abc\ndef' # \n includes another line
+print(s2)
+
+print(ord('a')) ## check ascii table
+
