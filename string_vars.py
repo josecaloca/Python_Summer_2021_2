5fa7e8bd7d3516ff44bc22c4c9cf005070ef5ebd
Jose Caloca <jose.caloca3@gmail.com>
2021-04-11 17:56:20 +0200

    prime numbers homework

--- string_vars.py
+++ string_vars.py
@@ -12,3 +12,21 @@ print(s2)
 
 print(ord('a')) ## check ascii table
 
+s4 = 'abc\tdef\t\t\thello'
+print(s4)
+
+s5 = 'abc\td\ef\t\t\te\gwg'
+print(s5)
+
+s6 = "student's"
+print(s6)
+
+s6 = 'student\'s'
+print(s6)
+
+s7 = '              asasasas asasasasas'
+print(s7.strip()) # eliminates leading spaces
+
+s5_tab = s5.split('\t')
+print(s5_tab )
+
