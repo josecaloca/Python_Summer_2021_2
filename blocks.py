981ebf77a183e6260e4f9fdb7f1d166fe4915e20
Jose Caloca <jose.caloca3@gmail.com>
2021-04-11 18:30:34 +0200

    variables blokcs

---
+++ blocks.py
@@ -0,0 +1,22 @@
+def sum(a, b):
+    c = 80
+    print('c =' + str(c))
+    return a + b
+
+a = 10
+b = 20
+
+if a > 5:
+    print('a>5')
+    print('b =' + str(b))
+else:
+    print('a<5')
+    print('b =' + str(b))
+
+print('b =' + str(b))
+
+print(sum(5,6))
+print()
+
+# variables inside a loop and inside a function are different to variables outside this internal environment
+
