4d2a72f0bcd291f46927b60fca5a5b6125ea5eb1
Jose Caloca <jose.caloca3@gmail.com>
2021-04-19 22:16:55 +0200

    Instructions

--- homeworks/stock_data.py
+++ homeworks/stock_data.py
@@ -1,3 +1,25 @@
+'''
+Instructions:
+1. Please download a CSV file containing the stock history of some companies, for example from:
+http://finance.yahoo.com/q/hp?s=GOOG
+http://finance.yahoo.com/q/hp?s=IBM
+http://finance.yahoo.com/q/hp?s=MSFT
+
+(Download Data)
+Save files giving them different names to a local folder on your drive
+
+2. Write a program that searches for CSV files with stock rates in a given folder and for every one of them:
+
+3. Calculates the percentage change betweeen Close and Open price and adds these values as another column to this CSV file.
+You can replace the old file or create a new one.
+
+Change = (Close-Open)/Open
+
+4. The output files can be stored in another folder
+5. You can use Python to download files. An example is given here: https://github.com/prubach/Python_Summer_2021_2/blob/master/download_file.py
+6. Please do not use pandas, or only use it as an alternative way of implementing it along a more "manual" way using just python without any libraries.
+'''
+
 import os
 import csv
 
